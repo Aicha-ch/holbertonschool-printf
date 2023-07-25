9322c25a47b3a89dc11cfe50137c4bb8b544ad40
abdel <holbertonc21@gmail.com>
2023-07-25 00:50:52 -0700

    update main.h and add prototypes

--- main.h
+++ main.h
@@ -13,9 +13,11 @@ typedef struct {
 
 
 
-
+void put_char(va_list args);
 int _printf(const char *format, ...);
-
+char *print_string (va_list args);
+void print_percentage();
+int print_integer (va_list args);
 
 
 #endif
